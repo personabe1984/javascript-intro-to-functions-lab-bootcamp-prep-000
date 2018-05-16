3a91c3c06a670adbdcd23e7cf566f822af153b35
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-16 18:00:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function shout(string) {
 }
 
 
-whisper(string)
+function whisper(string) {
+  //recieves one argument and returns it in all lower case
+}
+
+
+function logShout(string) {
+  //calls console.log() its one argument in all caps
+}
 
 sayHiToGrandma(string)
-
-logShout(string)

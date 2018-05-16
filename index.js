fc531a82d79da374faec73ea4c899da54199bd1f
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-16 18:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,23 +22,7 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!";
   }else if(string === shout(string)) {
     return "YES INDEED!";
-  }else if
-    
-  }
-  
-
-}
-
-
-function sayHiToGrandma(string) {
-  if(string === "I love you, Grandma."){
+  }else if(string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
-/*
-
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-*/
